3d46cf08bd7df47e51082b2ec4002acb88a60bfb
Tigercat1992 <silenceslider@gmail.com>
2021-04-02 00:22:34 +0630

    change default path in index.html

--- server/server.js
+++ server/server.js
@@ -453,13 +453,13 @@ app.post('/api/site/site_data',auth,admin,(req,res)=>{
 })
 
 
-// DEFAULT 
-// if( process.env.NODE_ENV === 'production' ){
-// 	const path = require('path');
-// 	app.get('/*',(req,res)=>{
-// 			res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
-// 	})
-// }
+DEFAULT 
+if( process.env.NODE_ENV === 'production' ){
+	const path = require('path');
+	app.get('/*',(req,res)=>{
+			res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+	})
+}
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
